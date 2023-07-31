d9868fbed421c588943ea5236169eac7771afc4c
Tanner Norris <jiujitsucarrot@Tanners-MacBook-Air.local>
2023-07-30 21:50:33 -0700

    Jokes and tips display on page after button is clicked.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,22 +5,57 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.getElementById("currentDay").textContent = formattedDate;
 
+    var jokeFetched = false;
+    var tipFetched = false;
+
+    document.querySelector(".joke").addEventListener("click", function () {
+        if (!jokeFetched) {
+            fetchJoke();
+            jokeFetched = true;
+        }
+    });
+
+    document.querySelector(".tips").addEventListener("click", function () {
+        if (!tipFetched) {
+            fetchTip();
+            tipFetched = true;
+        }
+    });
 });
 
-var response = fetch("https://official-joke-api.appspot.com/random_joke")
-    .then(function (response) { return response.json() })
-    .then(
-        function(data) {
-            console.log(data)
-        
-        }
-    )
+function updateTestContent(content) {
+    var testElement = document.getElementById("testContent");
+    testElement.innerHTML = content;
+}
 
-    var response = fetch("https://api.adviceslip.com/advice")
-    .then(function (response) { return response.json() })
-    .then(
-        function(data) {
-            console.log(data)
+function fetchJoke() {
+    fetch("https://official-joke-api.appspot.com/random_joke")
+        .then(function (response) {
+            return response.json();
+        })
         
-        }
-    )
+        .then(function (data) {
+            var joke = "<strong>Setup:</strong> " + data.setup + "<br><strong>Punchline:</strong> " + data.punchline;
+            updateTestContent(joke);
+        })
+        
+        .catch(function (error) {
+            console.log("Error fetching joke:", error);
+        });
+}
+
+function fetchTip() {
+    fetch("https://api.adviceslip.com/advice")
+        .then(function (response) {
+            return response.json();
+        })
+        
+        .then(function (data) {
+            var tip = "<strong>Today's tip:</strong> " + data.slip.advice;
+            updateTestContent(tip);
+        })
+        
+        .catch(function (error) {
+            console.log("Error fetching tip:", error);
+        });
+}
